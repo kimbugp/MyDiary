99754e2cb928af475e50f9226baa7de3003cfa87
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-07-20 23:28:01 +0300

    make tests to pass

--- app/v1/all_entries_test.py
+++ app/v1/all_entries_test.py
@@ -1,14 +1,18 @@
 import unittest
 import json
-from v1 import app
+from app.v1 import app
 
 class all_entries_test(unittest.TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
     #test if all entries are displayed
-    def display_all_entries(self):
-        test = app.test_client(self)
-        response = test.get("/entries", content_type="application/json")
+    def test_get_all_entries(self):
+        """Tests that all entries can be retrieved"""
+        testing_user = app.test_client(self)
+        response = testing_user.get("/entries", content_type="application/json")
         assert b'1' in response.data
         assert b'3' in response.data
+
+if __name__ == '__main__':
+    unittest.main()
