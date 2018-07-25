36332eb3cafbc8cc10dae147be326d3a5a60f4c0
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-07-25 13:17:19 +0300

    Change put and post endpointRecieve dta via forms for post and putchange app route for /api/v1 to json form

--- tests/all_entries_test.py
+++ tests/all_entries_test.py
@@ -39,14 +39,14 @@ class all_entries_test(unittest.TestCase):
     def test_get_all_entries(self):
         # Tests that all entries can be retrieved
         testing_user = app.test_client(self)
-        response = testing_user.get("/entries", content_type="application/json")
+        response = testing_user.get("/api/v1/entries", content_type="application/json")
         assert b'1' in response.data
         assert b'3' in response.data
-    
+
     def test_API_can_make_new_entry(self):
         # Tests for a new entry
         testing_user = app.test_client(self)
-        response = testing_user.post('/entries', data=json.dumps(self.test_entry[0]),
+        response = testing_user.post('/api/v1/entries', data=json.dumps(self.test_entry[0]),
                                      content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertIn(self.test_entry[0]['entry_name'], str(response.data),msg="No entry made")
@@ -60,20 +60,20 @@ class all_entries_test(unittest.TestCase):
         # Tests getting aspecific entry
         testing_user = app.test_client(self)
         # add test entry
-        response = testing_user.post('/entries', data=json.dumps(self.test_entry[0]),
+        response = testing_user.post('/api/v1/entries', data=json.dumps(self.test_entry[0]),
                                     content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertIn('Your memory entitled ' + self.test_entry[0]['title'] + ' has been saved', str(response.data))
         #view added entry
         id = {'entry_id': 1}
-        response = testing_user.get('/entries/{}'.format(id['entry_id']))
+        response = testing_user.get('/api/v1/entries/{}'.format(id['entry_id']))
         self.assertEqual(response.status_code, 200)
     
     def test_null_entry_(self):
         # api to fail if null entry
         testing_user = app.test_client(self)
         self.test_entry[0]['content'] = ''
-        response = testing_user.put('entries/1', data=json.dumps(self.test_entry[0]),
+        response = testing_user.put('/api/v1/entries/1', data=json.dumps(self.test_entry[0]),
                                     content_type='application/json')
         self.assertIn('Null entry', str(response.data))
 
