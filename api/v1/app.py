05e3e800f36bcfaa4902bdaf9ed50c2ce6652db7
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-07-26 23:06:56 +0300

    Create function to improve maitainability

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,15 @@ from flask import Flask,jsonify,make_response,request
 entries=[]
 app=Flask(__name__)
 
+def entry(var):
+    entry = {
+            'entry_id':len(entries) + 1,
+            'entry_date': var['entry_date'],
+            'entry_name': var['entry_name'],
+            'entry_content':var['entry_content']
+        }
+    return entry
+
 
 @app.route('/')
 def index():
@@ -15,16 +24,12 @@ def get_all_entries():
     if request.method=="GET":
         return make_response(jsonify({'entries':entries})),200
 
+
 @app.route('/api/v1/entries', methods=['POST'])
 def make_new_entry():
     if request.method=="POST":
         data=request.json
-        new_entry = {
-            'entry_id':len(entries) + 1,
-            'entry_date': data['entry_date'],
-            'entry_name': data['entry_name'],
-            'entry_content':data['entry_content']
-        }
+        new_entry=entry(data)
         entries.append(new_entry)
         return make_response(jsonify({'Message':new_entry})),201
 
@@ -42,12 +47,7 @@ def single_entry(entry_no):
 def edit_an_entry_(entry_no):
     if request.method=="PUT":
         data=request.json
-        update = {
-            'entry_id':len(entries) + 1,
-            'entry_date': data['entry_date'],
-            'entry_name': data['entry_name'],
-            'entry_content':data['entry_content']
-        }
+        update=entry(data)
         result = [entry for entry in entries if entry['entry_id'] == entry_no]
         if result:
             result[0]['entry_name'] = update['entry_id']
