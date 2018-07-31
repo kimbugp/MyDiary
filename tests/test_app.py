0de567dc320528cbee46fcea3d2354f9197ad5b8
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-07-31 14:00:05 +0300

    Add travis postgresql configuration

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,15 @@ from api import app
 from api.v1.models import dbase
 from api.v1.dbtasks import dboperations
 
+database=dboperations()
+db = dbase()
+'''Dummy Data'''
+test_user = {
+        "username":"simon",
+        "name":"Simon Peter" ,
+        "email": "kimbugwe@gmail.com",
+        "password":"12345678"
+    }
 
 class all_entries_test(unittest.TestCase):
 
@@ -14,6 +23,18 @@ class all_entries_test(unittest.TestCase):
         test_user = app.test_client(self)
         response = test_user.get(
             "/api/v1/entries", content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+
+
+class user_tests(unittest.TestCase):
+
+    # def setUp(self):
+        # db.conn('postgres://postgres:postgres@localhost:5432/diarydb_test')
+
+    def test_user_signup(self):
+        test_user = app.test_client(self)
+        response = test_user.post(
+            "/api/v1/auth/signup", data=json.dumps(test_user), content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
 
