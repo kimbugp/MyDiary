5b457fe8f6d965d347c7dffe8c84410ef239eba1
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-08 07:24:41 +0300

    Add heroku url

--- config.py
+++ config.py
@@ -12,16 +12,17 @@ class Config:
 class DevelopmentConfig(Config):
     """Class to configure database to developer"""
     db_name = 'diarydb'
-    # DEBUG = True
-    # # TESTING = False
     DATABASE_URL = 'postgresql://' + db_user + ': ' + \
-        user_password + '@localhost/' + db_name + ''
+        user_password + '@localhost/' + db_name + \
+        '' or "postgres://zjmricklwnsavr:1842468d\
+        23fa9a32e29cccbec9163b97cd4480ef\
+        949084cc7524fdf9cccc4a20@ec2-54-221-210-97.compute-1.amazonaws.com:\
+        5432/dcqilp2it5cbhp"
 
 
 class TestingConfig(Config):
     """Class to configure database to testing"""
     db_name = 'diarydb_test'
-    # DEBUG = True
     TESTING = True
     DATABASE_URL = 'postgresql://' + db_user + ': ' + \
         user_password + '@localhost/' + db_name + ''
@@ -31,6 +32,3 @@ app_config = {
     "development": DevelopmentConfig,
     "testing": TestingConfig
 }
-
-# def configure_app(configuration):
-#     app.config.from_object(configuration)
