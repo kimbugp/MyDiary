e4ba2340f775236fb8b2516717b3c82f85a6dc7e
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-11 11:17:54 +0300

    Add structure for the javascript

--- es6/js/main.js
+++ es6/js/main.js
@@ -5,3 +5,21 @@ var init={method:'get',headers:myheaders};
 var mybody=JSON.stringify({a: 7, str: 'Some string: &=&'})
 fetch(myURL,init,mybody)
 .then(res=>res.json()).then(res=>console.log(res));
+
+// function to post signup info
+
+
+//function to post signin info
+
+// function to post an entry
+
+//function to get all entries
+
+// function to et individula entry
+
+//function to delete an entry
+
+//function to edit entry
+
+
+//function to view profile
