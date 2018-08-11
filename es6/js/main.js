807f3fa9b9f431c513100a618c38b6a4e013a61d
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-11 12:04:09 +0300

    Add function for hello wrold and make a user

--- es6/js/main.js
+++ es6/js/main.js
@@ -1,13 +1,29 @@
 var fetch = require("node-fetch");
-var myURL='http://127.0.0.1:5000';
-var myheaders={'Content-Type': 'application/json'};
-var init={method:'get',headers:myheaders};
-var mybody=JSON.stringify({a: 7, str: 'Some string: &=&'})
-fetch(myURL,init,mybody)
-.then(res=>res.json()).then(res=>console.log(res));
+function hello_world(){
+    var base='http://127.0.0.1:5000';
+    var myheaders={'Content-Type': 'application/json'};
+    var init={method:'get',headers:myheaders};
+    fetch(base,init)
+    .then(res=>res.json()).then(res=>console.log(res));
+}
 
 // function to post signup info
-
+function create_user(){
+    var mybody=JSON.stringify({
+        "username": "petersimn",
+        "name": "Simon Peter",
+        "email": "kimbuge@gmail.com",
+        "password": "12345678"
+    })
+    var myURL='http://127.0.0.1:5000/api/v1/auth/signup';
+    var myheaders={'Content-Type': 'application/json'};
+    var init={method:'POST',headers:myheaders,body:mybody};
+    fetch(myURL,init)
+    .then(res=>res.json()).then(res=>console.log(res))
+    .catch(error => console.error(`Fetch Error =\n`, error));
+}
+hello_world()
+create_user()
 
 //function to post signin info
 
