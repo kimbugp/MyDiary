4fe1bddaeb566e266f87ab5c89645d6879db83dd
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-11 17:46:16 +0300

    Make index a form

--- es6/js/main.js
+++ es6/js/main.js
@@ -1,10 +1,26 @@
-var fetch = require("node-fetch");
+
+// var fetch = require("node-fetch");
+function submitForm(e, form){
+    e.preventDefault();
+    var body={
+        method: 'post',
+        body: JSON.stringify({name: form.username.value, password: form.password.value})};
+    fetch(body)
+    .then(function(response){return response.json();})
+    .then(function(data) {alert('form submited')});
+        // var mybody=JSON.stringify({
+        //     "username": "petersimn",
+        //     "password": "12345678"
+        // })
+    //   return data;
+}
 function hello_world(){
     var base='http://127.0.0.1:5000';
     var myheaders={'Content-Type': 'application/json'};
     var init={method:'get',headers:myheaders};
     fetch(base,init)
-    .then(res=>res.json()).then(res=>console.log(res));
+    .then(res=>res.json())
+    .then(res=>console.log(res));
 }
 
 // function to post signup info
@@ -22,9 +38,15 @@ function create_user(){
     .then(res=>res.json()).then(res=>console.log(res))
     .catch(error => console.error(`Fetch Error =\n`, error));
 }
-hello_world()
-create_user()
-
+function login(){
+    var mybody=submitForm();
+    var myURL='http://127.0.0.1:5000/api/v1/auth/login';
+    var myheaders={'Content-Type': 'application/json'};
+    var init={method:'POST',headers:myheaders,body:mybody};
+    fetch(myURL,init)
+    .then(res=>res.json()).then(res=>console.log(res))
+    .catch(error => console.error(`Fetch Error =\n`, error));
+}
 //function to post signin info
 
 // function to post an entry
