f860bd06cc740e3298c204d1dbba34fbe41195c7
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-12 08:30:01 +0300

    remove commented code

--- es6/js/main.js
+++ es6/js/main.js
@@ -1,23 +1,5 @@
 
 // var fetch = require("node-fetch");
-// function submitForm(e, form){
-//     e.preventDefault();
-//     var body={
-//         method: 'post',
-//         body: JSON.stringify({name: form.username.value, password: form.password.value})};
-//     fetch(body)
-//     .then(function(response){return response.json();})
-//     .then(function(data) {alert('form submited')})
-//     .catch(function(err) {alert('Error')});
-// }
-// function process_form(){
-//     var uname=document.getElementById('username').value;
-//     var pword=document.getElementById('password').value;
-//     var mybody=JSON.stringify({username:uname,password:pword});
-//     console.log(mybody);
-//     return mybody;
-
-// }
 function hello_world(){
     var base='http://127.0.0.1:5000';
     var myheaders={'Content-Type': 'application/json'};
@@ -40,10 +22,8 @@ function create_user(){
     var init={method:'POST',headers:myheaders,body:mybody};
     fetch(myURL,init)
     .then(res=>res.json()).then(res=>console.log(res));
-    // .catch(error => console.error(`Fetch Error =\n`, error));
 }
 function login(){
-    // var mybody=process_form();
     let mybody=JSON.stringify({
         "username": document.getElementById('username').value,
         "password": document.getElementById('password').value
@@ -57,7 +37,6 @@ function login(){
     .catch(error => console.error(`Fetch Error =\n`, error));
     return false;
 }
-// login()
 //function to post signin info
 
 // function to post an entry
