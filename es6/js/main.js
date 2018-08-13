6ffa65e9495b86bd2af7587787ab743b0f58fc1b
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-14 02:43:17 +0300

    redirect to index with no auth

--- es6/js/main.js
+++ es6/js/main.js
@@ -1,7 +1,7 @@
-var baseurl = 'https://simondb.herokuapp.com';
+// var baseurl = 'https://simondb.herokuapp.com';
 var Token = sessionStorage.getItem('Token');
 if(Token != true){
-	window.location.replace('401.html');
+	window.location.replace('index.html');
 } 
 // function hello_world() {
 // 	var myheaders = {
@@ -16,69 +16,7 @@ if(Token != true){
 // 		.then(res => console.log(res));
 // }
 
-// function to post signup info
-function create_user() {
-	var mybody = JSON.stringify({
-		'username': document.getElementById('username').value,
-		'name': document.getElementById('name').value,
-		'email': document.getElementById('email').value,
-		'password': document.getElementById('password').value
-	});
-	var myURL = baseurl + '/api/v1/auth/signup';
-	var myheaders = {
-		'Content-Type': 'application/json'
-	};
-	var init = {
-		method: 'POST',
-		headers: myheaders,
-		body: mybody
-	};
-	fetch(myURL, init)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (json) {
-			if (json.Message == 'User created') {
-				login();
-			} else if (json.Message == 'User already exists') {
-				document.getElementById('message').innerHTML = json.Message;
-			} else if (json.Message == 'invalid input') {
-				document.getElementById('message').innerHTML = json.Message;
-			}
-		})
-		.catch(error => console.error('Fetch Error =\n', error));
-	return false;
-}
 
-function login() {
-	let mybody = JSON.stringify({
-		'username': document.getElementById('username').value,
-		'password': document.getElementById('password').value
-	});
-	var myURL = baseurl + '/api/v1/auth/login';
-	var myheaders = {
-		'Content-Type': 'application/json',
-		'Accept': 'application/json'
-	};
-	var init = {
-		method: 'POST',
-		headers: myheaders,
-		body: mybody
-	};
-	fetch(myURL, init)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (json) {
-			if (json.Token) {
-				sessionStorage.setItem('Token', json.Token);
-				window.location.href = 'home.html';
-			}
-
-		})
-		.catch(error => console.error('Fetch Error =\n', error));
-	return false;
-}
 //function to post signin info
 
 // function to post an entry
