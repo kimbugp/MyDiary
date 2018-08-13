8e5df6e214b35e0f5bb6d9ea400779f56fcf07c6
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-13 12:05:14 +0300

    Add function to dispaly entries details

--- es6/js/home.js
+++ es6/js/home.js
@@ -21,28 +21,25 @@ function get_entries(){
         var object=json;
         var objectlength=object.entries.length;
         for (var i = 0; i < objectlength; i++) {
-            show_data(object.entries[i].entry_name);
-            // for (const [key, value] of Object.entries(object.entries[i])) {
-            //     // console.log(key,value);
-            //     show_data(value);
-            //   }
+            show_data(object.entries[i].entry_name,object.entries[i].entry_date,object.entries[i].entry_content);
         }
     })
     .catch(error => console.error(`Fetch Error =\n`, error));
     return false;
 }
-function show_data(value){
+function show_data(title,d,content){
     var record = document.createElement('li');
-    record.appendChild(document.createTextNode(value));
+    record.appendChild(document.createTextNode(title));
     document.getElementById('myUL').appendChild(record);
-
     // Get the modal
     var modal = document.getElementById('myModal');
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-
     record.onclick=function(){
         modal.style.display = "block";
+        document.getElementById('entry_title').innerHTML=title;
+        document.getElementById('entry_content').innerHTML=content;
+        document.getElementById('date').innerHTML=d;
     }
     span.onclick = function() {
         modal.style.display = "none";
