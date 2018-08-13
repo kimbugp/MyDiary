b965ece23bfec5918420edb2cae71b7e8889568c
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-14 01:48:56 +0300

    Correct styling

--- es6/js/token.js
+++ es6/js/token.js
@@ -1,9 +1,9 @@
-var links = document.getElementsByClassName("navbar");
+var links = document.getElementsByClassName('navbar');
 for (var i = 0; i < links.length; i++) {
-    var text = links[i].textContent;
-    links[i].textContent = "";
-    var a = document.createElement("a");
-    a.href = text.toLowerCase() + ".html";
-    a.textContent = text;
-    links[i].appendChild(a);
+	var text = links[i].textContent;
+	links[i].textContent = '';
+	var a = document.createElement('a');
+	a.href = text.toLowerCase() + '.html';
+	a.textContent = text;
+	links[i].appendChild(a);
 }
