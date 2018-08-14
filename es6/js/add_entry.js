961b85f1dd7303a508d49a4ce770c9dbed67ed2e
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-14 21:26:22 +0300

    Add UI functionality to add entry

--- es6/js/add_entry.js
+++ es6/js/add_entry.js
@@ -1,9 +1,11 @@
-function add_entry(id) {
+// from home import baseurl
+const baseurl = 'https://simondb.herokuapp.com';
+const Token = sessionStorage.getItem('Token');
+
+function add_entry() {
 	var mybody = JSON.stringify({
-		'username': document.getElementById('username').value,
-		'name': document.getElementById('name').value,
-		'email': document.getElementById('email').value,
-		'password': document.getElementById('password').value
+		'entry_content': document.getElementById('new_entrycontent').value,
+		'entry_name': document.getElementById('new_entryname').value,
 	});
 	let myURL = baseurl + '/api/v1/entries/';
 	let myheaders = {
@@ -21,10 +23,30 @@ function add_entry(id) {
 			return response.json();
 		})
 		.then(function (response) {
-			location.reload();
+			if (response.Message == 'entry created') {
+				alert(response.Message);
+				location.reload();
+			}
+			alert(response.Message);
 		})
 		.catch(error => {
 			alert(error);
 		});
 	return false;
+}
+//close modal when clicked
+close_newentrymodel();
+
+function close_newentrymodel() {
+	var modal = document.getElementById('newentrymodal');
+	let span = document.getElementsByClassName('close')[1];
+	span.onclick = function () {
+		modal.style.display = 'none';
+	};
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = 'none';
+		}
+	};
 }
