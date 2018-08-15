2092515d1264e634698631a99249d987e6f40b4b
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-15 19:08:42 +0300

    add loader to signin

--- es6/js/loader.js
+++ es6/js/loader.js
@@ -1,6 +1,9 @@
 function loader(state) {
 	if (state == true) {
-		document.getElementById('loader').style.display = 'block';
+		document.getElementById('loader').style.display = 'inline-block';
+		var styleElem = document.head.appendChild(document.createElement('style'));
+		styleElem.innerHTML = '#loader:after{display:"block"}';
+        
 	} else if (state == false) {
 		document.getElementById('loader').style.display = 'none';
 	}
