4d9da407bf015aa9a93a6f5a507b239c114eb006
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-16 08:01:25 +0300

    Change backgroud size

--- es6/js/loader.js
+++ es6/js/loader.js
@@ -8,3 +8,28 @@ function loader(state) {
 		document.getElementById('loader').style.display = 'none';
 	}
 }
+
+
+// function errorbox(){
+// 	let errorbox=document.getElementById('errormsg');
+// 	var errorheader=document.createElement('div')
+// 	errorheader.className('entry_name')
+// 	<div id="myModal" class="entry_name">
+//         <!-- Modal content -->
+//         <div class="modal-content">
+//             <div class="entry_name">
+//                 <span class="close">&times;</span>
+//                 <h2 id='entry_title'>title</h2>
+//             </div>
+//             <div class="modal-body">
+//                 <p id='entry_content'>Some text in the Modal Body</p>
+//             </div>
+//             <div class="entry_date">
+//                 <span class="action">&#x270E;</span>
+//                 <span class="action">&#x1F5D1;</span>
+//                 <h3 id='date'>entry_date</h3>
+//             </div>
+//         </div>
+
+//     </div>
+// }
