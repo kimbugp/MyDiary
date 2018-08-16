e5b16fa7ff36d97c18dc8ce62509d7f044ea8837
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-16 19:45:26 +0300

    Change to heroku address

--- es6/js/main.js
+++ es6/js/main.js
@@ -1,6 +1,6 @@
 
-// const baseurl = 'https://simondb.herokuapp.com';
-const baseurl = 'http://127.0.0.1:5000';
+const baseurl = 'https://simondb.herokuapp.com';
+// const baseurl = 'http://127.0.0.1:5000';
 const Token = sessionStorage.getItem('Token');
 if(Token == null){
 	window.location.href='index.html';
