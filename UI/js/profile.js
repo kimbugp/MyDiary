e8c798fbc5813d5afe6c53de7abeb95e44113d47
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-27 21:00:39 +0300

    add function to edit profile

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -2,7 +2,7 @@
 function get_profile() {
 	let url = '/api/v1/profile';
 	let profile=new Profile(url);
-	profile.getprofile();
+	profile.get();
 }
 function displayprofile(no,mail,username,name){
 	let entries=document.getElementById('noentries');
@@ -17,5 +17,10 @@ function displayprofile(no,mail,username,name){
 	let uname=document.getElementById('uname');
 	uname.innerText='Username:'+username;
 
+}
+function editprofile(){
+	let url='/api/v1/profile';
+	let profile=new Profile(url);
+	profile.edit();
 }
 get_profile();
