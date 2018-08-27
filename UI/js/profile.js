8978cea29e1af23498b576519de33b8d31000483
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-28 00:30:12 +0300

    test function to edit profile

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -19,8 +19,14 @@ function displayprofile(no,mail,username,name){
 
 }
 function editprofile(){
+	let key='username';
+	let value='kimbugwe';
 	let url='/api/v1/profile';
 	let profile=new Profile(url);
-	profile.edit();
+	profile.edit(key,value);
+	get_profile();
 }
+
+document.getElementById('fullname').setAttribute('onclick','editprofile();');
+document.getElementById('useremail').setAttribute('onclick','editprofile();');
 get_profile();
