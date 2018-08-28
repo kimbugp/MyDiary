f0316556b990e7bf18987bbefe9ed2c711cd0bc5
kimbugwe simon peter <kimbsimon2@gmail.com>
2018-08-28 16:47:31 +0300

    change function to edit profession

--- UI/js/profile.js
+++ UI/js/profile.js
@@ -18,17 +18,12 @@ function displayprofile(no,mail,username,name){
 	uname.innerText='Username:'+username;
 
 }
-// function editprofile(){
-// 	let key='username';
-// 	let value='kimbugwe';
-// 	let url='/api/v1/profile';
-// 	let profile=new Profile(url);
-// 	profile.edit(key,value);
-// 	get_profile();
-// }
-// //set onclick events to edit profile
-// document.getElementById('name').setAttribute('onclick','editprofile();');
-// document.getElementById('email').setAttribute('onclick','editprofile();');
+function editprofile(){
+	let url='/api/v1/profile';
+	let profile=new Profile(url);
+	profile.edit(key,value);
+	get_profile();
+}
 
 
 get_profile();
