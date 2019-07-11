c30bd9e2c655c82e5f76afc9ed86383e3005d22b
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-07-11 16:50:12 +0300

    Update main.py
    add cors

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from config import app_config
 from flask import Flask
 from app.models.models import MODELS
+from flask_cors import CORS
 
 
 def create_app(config):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config.get(config))
     app.url_map.strict_slashes = False
-
+    
+    # add cors
+    CORS(app)
     # bind app to db
     db = MODELS()
     db.create_user_table()
