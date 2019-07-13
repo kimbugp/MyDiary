33c58b2df1ef4a707e8d8c2f07f16a7a69cd6852
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-07-13 12:05:39 +0300

    restructure tests

---
+++ tests/helpers.py
@@ -0,0 +1,215 @@
+import json
+from tests import (test_entry, test_sign_in, test_user_data,
+                   test_wrong_sign_in, wrong_test_entry, wrong_test_sign_in,
+                   wrong_test_user_data)
+from app.models.dbtasks import Profile, UserOperations
+database = UserOperations()
+
+
+class Helpers():
+    def __init__(self, test_user):
+        self.test_user = self.test_user
+
+    def user_create_token(self):
+        """FUnction to create login token"""
+        self.test_user.post(
+            "/api/v1/auth/signup", data=json.dumps(
+                test_user_data), content_type="application/json")
+        response = self.test_user.post('/api/v1/auth/login',
+                                       data=json.dumps(test_sign_in),
+                                       content_type='application/json')
+        token = response.json
+        return token
+
+    def user(self):
+        """FUnction to signin"""
+        self.test_user.post(
+            "/api/v1/auth/signup", data=json.dumps(
+                test_user_data), content_type="application/json")
+        response = self.test_user.post('/api/v1/auth/login',
+                                       data=json.dumps(test_sign_in),
+                                       content_type='application/json')
+        return response
+
+    def user_create(self):
+        """Function to create a user"""
+        response = self.test_user.post(
+            "/api/v1/auth/signup", data=json.dumps(test_user_data),
+            content_type="application/json")
+        return response
+
+    def wrong_details(self):
+        """Function test login with missing login info"""
+        response = self.test_user.post(
+            "/api/v1/auth/login", data=json.dumps(wrong_test_sign_in),
+            content_type="application/json")
+        return response
+
+    def create_an_entry(self):
+        """Function to create an entry"""
+        response = self.test_user.post('/api/v1/entries',
+                                       headers=self.user_create_token(),
+                                       data=json.dumps(test_entry),
+                                       content_type='application/json')
+        return response
+
+    def get_an_entry(self):
+        """Function to all get an entry"""
+        response = self.test_user.get(
+            "/api/v1/entries", headers=self.user_create_token(),
+            content_type="application/json")
+        return response
+
+    def wrong_user(self):
+        """Function to test wrong user input"""
+        response = self.test_user.post(
+            "/api/v1/auth/signup", data=json.dumps(wrong_test_user_data),
+            content_type="application/json")
+        return response
+
+    def create_wrong_entry(self):
+        """Function to test wrong user entry input"""
+        response = self.test_user.post('/api/v1/entries',
+                                       headers=self.user_create_token(),
+                                       data=json.dumps(wrong_test_entry),
+                                       content_type='application/json')
+        return response
+
+    def get_single_entry(self):
+        """Function to all get an entry"""
+        my_id = database.get_an_id()
+        response = self.test_user.get('/api/v1/entries/{}'.format(my_id),
+                                      headers=self.user_create_token(),
+                                      content_type='application/json')
+        return response
+
+    def no_existing_entry(self):
+        """Function to test non exisitng entry"""
+        response = self.test_user.get(
+            "/api/v1/entries/1", headers=self.user_create_token(),
+            content_type="application/json")
+        return response
+
+    def edit_entry(self):
+        """Function to test editing an entry"""
+        self.create_an_entry()
+        my_id = database.get_an_id()
+        response = self.test_user.put('/api/v1/entries/{}'.format(my_id),
+                                      headers=self.user_create_token(),
+                                      data=json.dumps(test_entry),
+                                      content_type='application/json')
+        return response
+
+    def delete_entry(self):
+        """Function to test deleting an entry"""
+        self.create_an_entry()
+        my_id = database.get_an_id()
+        response = self.test_user.delete('/api/v1/entries/{}'.format(my_id),
+                                         headers=self.user_create_token(),
+                                         data=json.dumps(test_entry),
+                                         content_type='application/json')
+        return response
+
+    def no_token(self):
+        """Function to test operation with no token"""
+        self.create_an_entry()
+        my_id = database.get_an_id()
+        response = self.test_user.put('/api/v1/entries/{}'.format(my_id),
+                                      data=json.dumps(test_entry),
+                                      content_type='application/json')
+        return response
+
+    def wrong_token(self):
+        """Function to test operation with wrong token"""
+        self.create_an_entry()
+        my_id = database.get_an_id()
+        response = self.test_user.put('/api/v1/entries/{}'.format(my_id),
+                                      headers={"token": "ddddd"},
+                                      data=json.dumps(test_entry),
+                                      content_type='application/json')
+        return response
+
+    def wrong_sign_in(self):
+        """Function to test sign in with wrong details"""
+        response = self.test_user.post(
+            "/api/v1/auth/login", data=json.dumps(test_wrong_sign_in),
+            content_type="application/json")
+        return response
+
+    def edit_no_entry(self):
+        """Function to test trying to edit non existng entry"""
+        self.create_an_entry()
+        response = self.test_user.put('/api/v1/entries/1',
+                                      headers=self.user_create_token(),
+                                      data=json.dumps(test_entry),
+                                      content_type='application/json')
+        return response
+
+    def wrong_data(self):
+        """Function to test trying make wrong entry"""
+        self.create_an_entry()
+        my_id = database.get_an_id()
+        response = self.test_user.put('/api/v1/entries/{}'.format(my_id),
+                                      headers=self.user_create_token(),
+                                      data=json.dumps(wrong_test_entry),
+                                      content_type='application/json')
+        return response
+
+    def helo(self):
+        """Function to test hello world"""
+        response = self.test_user.get('/', content_type="application/json")
+        return response
+
+    def error_page(self):
+        """Function to test 404 errors"""
+        response = self.test_user.get('/3/', content_type="application/json")
+        return response
+
+    def profile(self):
+        """Function to test retruning profile"""
+        response = self.test_user.get('/api/v1/profile',
+                                      headers=self.user_create_token(),
+                                      content_type='application/json')
+        return response
+
+    def edit_profession(self):
+        """Function to test edit profile profession"""
+        response = self.test_user.put('/api/v1/profile',
+                                      headers=self.user_create_token(),
+                                      data=json.dumps({'profession': 'Simon'}),
+                                      content_type='application/json')
+        return response
+
+    def edit(self):
+        """Function to test no parameter sent to edit profile"""
+        response = self.test_user.put('/api/v1/profile',
+                                      headers=self.user_create_token(),
+                                      content_type='application/json')
+        return response
+
+    def pic_upload(self):
+        """Function to test add pic"""
+        data = {}
+        data['photo'] = (io.BytesIO(b'test'), 'test_file.jpg')
+        response = self.test_user.post('/api/v1/profile/pic',
+                                       headers=self.user_create_token(),
+                                       data=data,
+                                       content_type='multipart/form-data')
+        return response
+
+    def pic_no_upload(self):
+        """Function to test not add pic"""
+        response = self.test_user.post('/api/v1/profile/pic',
+                                       headers=self.user_create_token(),
+                                       data=json.dumps({"path": ""}),
+                                       content_type='application/json')
+        return response
+
+    def pic_not_added_upload(self):
+        """Function to test add not pic"""
+        data = {}
+        response = self.test_user.post('/api/v1/profile/pic',
+                                       headers=self.user_create_token(),
+                                       data=data,
+                                       content_type='multipart/form-data')
+        return response
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,61 +1,59 @@
 """Module to test login/signup user activity """
 import unittest
 
-from tests.base import (TestingClass, edit, edit_profession, helo,
-                        pic_no_upload, pic_not_added_upload, pic_upload, user,
-                        user_create, wrong_details, wrong_sign_in, wrong_user)
+from tests.helpers import Helpers
+from tests.base import TestingClass
 
 
-class UserTests(TestingClass):
+class UserTests(TestingClass, Helpers):
     """Class to test endpoints"""
 
     def test_user_signup(self):
         """Method to test signup"""
-        response = user_create(self.test_user)
+        response = self.user_create()
         self.assertEqual(response.status_code, 201)
         self.assertIn('User created', str(response.data))
 
     def test_user_signup_wrong(self):
         """Method to test wrong signup"""
-        response = wrong_user(self.test_user)
+        response = self.wrong_user()
         self.assertEqual(response.status_code, 400)
 
     def test_user_already_exists(self):
         """Method to test existing user"""
-        user_create(self.test_user)
-        response = user_create(self.test_user)
+        self.user_create()
+        response = self.user_create()
         self.assertEqual(response.status_code, 400)
         self.assertIn('User already exists', str(response.data))
 
     def test_can_login_user(self):
         """Method to test signin"""
-        response = user(self.test_user)
+        response = self.user()
         self.assertEqual(response.status_code, 200)
-    
+
     def test_create_user_with_invalid_email(self):
-        response = wrong_user(self.test_user)
+        response = self.wrong_user()
         self.assertEqual(response.status_code, 400)
 
     def test_user_login_with_no_info(self):
         """Method to test sign in with missing parameter"""
-        response = wrong_details(self.test_user)
+        response = self.wrong_details()
         self.assertIn(response.json['error'],
                       "'password' is a required property")
 
     def test_wrong_user_login(self):
         """Method to test sign in with wrong detail"""
-        response = wrong_sign_in(self.test_user)
+        response = self.wrong_sign_in()
         self.assertEqual(response.status_code, 401)
         self.assertIn('Invalid login', str(response.data))
 
     def test_hello_world(self):
         """Method to test hello world"""
-        response = helo(self.test_user)
+        response = self.helo()
         self.assertIn('hello', str(response.data))
         self.assertEqual(response.status_code, 200)
 
     def test_edit_profession(self):
         """Method to test editing profession"""
-        response = edit_profession(self.test_user)
+        response = self.edit_profession()
         self.assertEqual(response.status_code, 200)
-
