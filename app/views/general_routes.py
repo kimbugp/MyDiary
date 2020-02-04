27e1ac6988a5e1b241b89c8208848202f918a9db
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-04 17:02:39 +0300

    add s3 webhook slack

--- app/views/general_routes.py
+++ app/views/general_routes.py
@@ -1,17 +1,35 @@
-from flask import Blueprint, jsonify, make_response
+import json
+import os
+
+import requests
+from flask import Blueprint, jsonify, make_response, request, redirect
 from jsonschema import ValidationError as Error
 
+import boto3
 from app.utils.error_handlers import ValidationError
+from app.utils.s3 import copy_to_bucket
+import textwrap
+others = Blueprint("errors", __name__)
 
-others = Blueprint('errors', __name__)
 
-
-@others.route('/')
+@others.route("/")
 def index():
     """
     End Point for the index page
     """
-    return jsonify({'hello': 'world'}), 200
+    data = f'''client_id={os.environ.get('JOBS_SLACK_CLIENT_ID')}&client_secret={os.environ.get('JOBS_SLACK_CLIENT_SECRET')}&code={request.args.get('code')}'''
+    res = requests.post(
+        "https://slack.com/api/oauth.access",
+        data,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    response_json = res.json()
+    if response_json.get("ok") is False:
+        return redirect('https://slack.com/400', code=302)
+    copy_to_bucket(response_json.get("incoming_webhook"))
+    team = response_json.get('incoming_webhook').get(
+        'configuration_url').split('.slack.com')[0]
+    return redirect(team + '.slack.com', code=302)
 
 
 @others.app_errorhandler(404)
@@ -19,7 +37,7 @@ def page_not_found(e):
     """
     End Point to catch 404s
     """
-    return make_response(jsonify({'message': 'Page not found'})), 404
+    return make_response(jsonify({"message": "Page not found"})), 404
 
 
 @others.app_errorhandler(ValidationError)
@@ -31,6 +49,6 @@ def handle_invalid_usage(error):
 
 @others.app_errorhandler(Error)
 def handle_invalid_input(error):
-    response = jsonify({'error': error.message})
+    response = jsonify({"error": error.message})
     response.status_code = 400
     return response
